d2f6747898344eae71f9529578f4962358a399cb
Harshitha h <harshiarchu5@gmail.com>
2025-11-27 12:39:02 +0530

    js basic

---
+++ j.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple JavaScript Example</title>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <p>Click the button to see a message:</p>
+    <button onclick="showMessage()">Click Me</button>
+
+    <script>
+        function showMessage() {
+            alert("Hello! This is a simple JavaScript message.");
+        }
+    </script>
+</body>
+</html>
